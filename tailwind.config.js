e1ae7a3776cd440d408ea0cfcf7699b1cff652c2
Janssen Radh Yumang <janssenreal23@gmail.com>
2023-07-15 20:41:51 +0800

    📦️package: added router and tailwind icon

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#decdb5",
+        secondary: "#0f2224",
+        background: "#050505",
+        text: "#fafafa",
+        accent: "#95aed0",
+      },
+    },
   },
   plugins: [],
 };
